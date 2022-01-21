1a8627af3c7429ae0688964ec41925c9e3520c2d
Nur <raynura182@gmail.com>
2022-01-20 17:10:38 -0800

    serach by serv and title

--- JS-code/filter.js
+++ JS-code/filter.js
@@ -1,5 +1,5 @@
 import { getDataMado } from "./dataMado.js";
-import { showItems } from "./main.js";
+// import { showItems } from "./main.js";
 
 const dataMado = getDataMado();
 const container = document.querySelector(".container"); // change className
@@ -16,81 +16,3 @@ dataMado.forEach(function (el) {
 container.appendChild(info)
 })
 
-// filtr part Nur
-
-const navFilterBlock = document.querySelector(".nav-search-items");
-const itemTitle = document.querySelector("#itemTitle");
-const servedFor = document.querySelector("#servedFor");
-
-export function navFilter(filteredArray) {
-    navFilterBlock.style.display = "block";
-    filtrPrice();
-    itemTitleFn(filteredArray, itemTitle);
-    servedForFn(filteredArray, servedFor)
-}
-
-function servedForFn(filteredArray, dataList) {
-    while (dataList.firstChild) {
-        dataList.removeChild(dataList.firstChild);
-    }
-    const searchArray = [];
-    for (let i = 0; i < filteredArray.length; i++) {
-        let val = filteredArray[i].servedFor;
-        if (!searchArray.includes(val)) {
-            searchArray.push(val);
-            const d = document.createElement("div");
-            d.innerHTML = `<option value="${val}">`;
-            dataList.appendChild(d);
-        }
-    }
-}
-function itemTitleFn(filteredArray, dataList) {
-    while (dataList.firstChild) {
-        dataList.removeChild(dataList.firstChild);
-    }
-    const searchArray = [];
-    for (let i = 0; i < filteredArray.length; i++) {
-        let val = filteredArray[i].itemTitle;
-        if (!searchArray.includes(val)) {
-            searchArray.push(val);
-            const d = document.createElement("div");
-            d.innerHTML = `<option value="${val}">`;
-            dataList.appendChild(d);
-        }
-    }
-}
-
-
-document.querySelector('.servedForInput').addEventListener('input', function (event) {
-    const item = event.target.value;
-    console.log(item);
-    const filteredArray = dataMado.filter((el) => el.servedFor.includes(item));
-    showItems(filteredArray);
-})
-document.querySelector('.itemTitleInput').addEventListener('input', function (event) {
-    const item = event.target.value;
-    console.log(item);
-})
-// --- search in all  Data
-// const searchValue = document.querySelector(".allData");
-// export function searchAllData() {
-//     const val = searchValue.value;
-//     console.log(val);
-// }
-
-// ------ slidecontainer---
-const slideMin = document.getElementById("priceMin");
-const slideMax = document.getElementById("priceMax");
-const y = document.getElementById("vMin");
-const x = document.getElementById("vMax");
-// ------ slidecontainer--- ^^^
-export function filtrPrice() {
-    y.innerHTML = slideMin.value;
-    x.innerHTML = slideMax.value;
-    slideMin.oninput = function () {
-        y.innerHTML = this.value;
-    };
-    slideMax.oninput = function () {
-        y.innerHTML = this.value;
-    };
-}
