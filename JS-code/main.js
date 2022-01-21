aa0cbd4a49a7c56c0cecb7e36d0b41cbbfc5b82b
BekzatMinbaev <bekatut00@email.com>
2022-01-20 20:15:24 -0800

    fixed some styling in category part

--- JS-code/main.js
+++ JS-code/main.js
@@ -9,17 +9,7 @@ const gridContainer=document.querySelector("#grid-container")
 const menuId=document.querySelector(".menu-id")
 const searchByNames=document.querySelector("#search-bar")
 const searchBtn=document.querySelector("#searchBtn")
-// dataMado.forEach(function (el) {
-//     const info = document.createElement("div");
-//     info.innerHTML = `
-//     <h4>${el.menus}</h4>
-//     <img src="${el.imgURL}" alt="">
-//     <p>${el.itemTitle}</p>
-//     <p>${el.servedFor}</p>
-//     <p>${el.itemPrice}</p>
-// `
-// container.appendChild(info)
-// })
+
 
 // clear fucntion
 function clearContainer(){
@@ -30,24 +20,14 @@ function clearContainer(){
 // getting id from data
 function rightMenuAction(event){
 const item=event.target;
-// console.log(item.innerText)
 titleMenu.innerText=item.innerText
-// const filteredArray=[]
-const filteredArray = dataMado.filter(el => el.menus.toLowerCase().includes(item.id))
-// for(let i=0; i<dataMado.length; i++){
-//    const x = dataMado[i].menus.toLowerCase()
-//     if(x.includes(item.id)){
-//         filteredArray.push(dataMado[i])
-//         // console.log(dataMado[i].menus)
-//     }
-// }
-
-      
+const filteredArray = dataMado.filter(el => el.menus.toLowerCase().includes(item.id))     
 showItems(filteredArray)
 navFilter(filteredArray); // calling filter part and show
-// console.log(filteredArray)
+
 
 }
+
 //fucntion for each category
 export function showItems(arr){
     
@@ -55,17 +35,16 @@ export function showItems(arr){
     clearContainer()
     for(let i=0; i<arr.length;i++){
         const newDiv=document.createElement("div")
-        newDiv.className="menu-item"
-
+        newDiv.className="menu-items"
         newDiv.innerHTML=`
-        <a href="#" class="item-card">
-        <div class="item-info">
-         <img src="${arr[i].imgURL}" class="item-img">
+        <img src="${arr[i].imgURL}" class="item-img">
+        
+                     <div class="menu-items">
                              <div class="served-for">${arr[i].servedFor}</div>
                              <div class="item-name">${arr[i].itemTitle}</div>
                              <div class="item-price">${arr[i].itemPrice}</div>
                          </div>
-                     </a>
+                     
         `
        
         rightMenu.appendChild(newDiv)
@@ -92,7 +71,6 @@ return showItems(searchedArray)
 
 // event listeners
 searchBtn.addEventListener("click", searchButton )
-// searchByNames.addEventListener("click")
 menuId.addEventListener("click", menuDirection)
 gridContainer.addEventListener("click",rightMenuAction )
 leftMenu.addEventListener("click", rightMenuAction)
